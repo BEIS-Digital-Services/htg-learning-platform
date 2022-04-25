734afc4d7b4f3cfba1c458cc0d1bea6c08f66e0b
TasTasniem <97549043+TasTasniem@users.noreply.github.com>
2022-04-25 08:40:13 +0100

    New sample menu

---
+++ Beis.LearningPlatform.Web/Controllers/NewMenuController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Beis.LearningPlatform.Web.Controllers
+{
+    public class NewMenuController : Controller
+    {
+        public IActionResult home()
+        {
+            return View();
+        }
+
+        public IActionResult about()
+        {
+            return View();
+        }
+
+        public IActionResult aboutsub1()
+        {
+            return View();
+        }
+        public IActionResult aboutsub2()
+        {
+            return View();
+        }
+        public IActionResult aboutsub3()
+        {
+            return View();
+        }
+        public IActionResult aboutsub4()
+        {
+            return View();
+        }
+    }
+}
