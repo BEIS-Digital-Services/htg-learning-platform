58fcaed193374f1cd538d4ca5e111fa1d2aa6699
emailtokvk <karthik@belaku.co.uk>
2022-04-29 12:05:08 +0100

    support for local

--- Beis.LearningPlatform.Web/Program.cs
+++ Beis.LearningPlatform.Web/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.ApplicationInsights;
 using System;
+using System.Linq;
 
 namespace Beis.LearningPlatform.Web
 {
@@ -39,7 +40,13 @@ namespace Beis.LearningPlatform.Web
                    {
                        configuration.AddJsonFile("diagnosticForm.json", false, true);
                        var configurationRoot = configuration.Build();
-                       configuration.AddAzureAppConfiguration(configurationRoot.GetConnectionString("AppConfig"));
+
+                       var connectionString = configurationRoot.GetConnectionString("AppConfig");
+                       if (connectionString != null)
+                       {
+                           configuration.AddAzureAppConfiguration(connectionString);
+                       }
+
                        ApplicationForm options = new();
                        configurationRoot.GetSection(nameof(ApplicationForm)).Bind(options);
                    })
