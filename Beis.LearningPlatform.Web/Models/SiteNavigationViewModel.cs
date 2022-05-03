426e546ff4b1422e51e13b4f9152bde53db83747
Thomas Poulsen <thomas.poulsen@broadlight.io>
2022-05-03 13:56:41 +0100

    Wip

---
+++ Beis.LearningPlatform.Web/Models/SiteNavigationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Beis.LearningPlatform.Web.Interfaces;
+using System;
+
+namespace Beis.LearningPlatform.Web.Models
+{
+	public class SiteNavigationViewModel
+    {
+		private readonly IPageViewModel _pageViewModel;
+
+		public SiteNavigationViewModel(IPageViewModel pageViewModel)
+        {
+            
+            _pageViewModel = pageViewModel ?? throw new ArgumentNullException(nameof(pageViewModel));
+        }
+
+    }
+}
---
+++ Beis.LearningPlatform.Web/ViewComponents/SiteNavigationViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Beis.LearningPlatform.Web.Interfaces;
+using Beis.LearningPlatform.Web.Models;
+using Beis.LearningPlatform.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Beis.LearningPlatform.Web.ViewComponents
+{
+
+	public class SiteNavigationViewComponent : ViewComponent
+    {
+		private readonly ICmsService _cmsService;
+
+		public SiteNavigationViewComponent(ICmsService cmsService)
+        {
+            _cmsService = cmsService;
+        }
+
+        public IViewComponentResult Invoke(IPageViewModel pageViewModel)
+        {
+            var viewModel = new SiteNavigationViewModel(pageViewModel);
+            return View(viewModel);
+        }
+    }
+}
