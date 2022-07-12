a3f8539e9eb087491703a89e4c9cec326265eec3
Thomas Poulsen <thomas.poulsen@broadlight.io>
2022-07-12 16:13:34 +0100

    wip

---
+++ Beis.LearningPlatform.Web/ViewComponents/CmsExpressionOfInterestViewComponent.cs
@@ -0,0 +1,26 @@
+﻿namespace Beis.LearningPlatform.Web.ViewComponents
+{
+    public class CmsExpressionOfInterestViewComponent : ViewComponent
+    {        
+        private readonly MarkdownPipeline _markdownPipeline;
+
+        public CmsExpressionOfInterestViewComponent(MarkdownPipeline markdownPipeline)
+        {
+            _markdownPipeline = markdownPipeline;
+        }
+
+
+        public IViewComponentResult Invoke(CMSPageComponent cmsPageComponent)
+        {
+            var viewModel = new CmsExpressionOfInterestViewModel(cmsPageComponent);
+
+            if (viewModel.HasContent)
+            {
+                viewModel.FormIntroHtml = Markdown.ToHtml(cmsPageComponent.FormIntro, _markdownPipeline);
+                viewModel.ThankYouTextHtml = Markdown.ToHtml(cmsPageComponent.ThankYouText, _markdownPipeline);
+            }
+
+            return View(viewModel);
+        }
+    }
+}
