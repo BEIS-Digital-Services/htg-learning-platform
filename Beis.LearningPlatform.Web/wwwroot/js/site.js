60373dcdb4222b37c826cde7e1fee12075a84b9c
Sunil D'Souza <sunil.dsouza@angloamerican.com>
2022-07-28 17:31:12 +0100

    LP-2944 - Fix for missing aria-expanded attribute for accordian

--- Beis.LearningPlatform.Web/wwwroot/js/site.js
+++ Beis.LearningPlatform.Web/wwwroot/js/site.js
@@ -87,7 +87,11 @@ var i;
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
         this.classList.toggle("active");
+        let expanded = this.getAttribute('aria-expanded') === 'true';
+        this.setAttribute('aria-expanded', !expanded);
+        
         var panel = this.nextElementSibling;
+        panel.hidden = expanded;
         if (panel.style.display === "block") {
             panel.style.display = "none";
         } else {
