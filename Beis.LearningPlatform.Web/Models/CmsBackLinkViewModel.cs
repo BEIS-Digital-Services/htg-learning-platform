0128baa20aec59bcd50ff20464ab1434e2b12809
Thomas Poulsen <thomas.poulsen@broadlight.io>
2022-07-28 14:23:51 +0100

    LP-3152: CMS Back Link component

---
+++ Beis.LearningPlatform.Web/Models/CmsBackLinkViewModel.cs
@@ -0,0 +1,52 @@
+﻿namespace Beis.LearningPlatform.Web.Models
+{
+    public class CmsBackLinkViewModel
+    {
+        private readonly CMSPageComponent _cmsPageComponent;
+        private readonly string _linkId;
+
+        public CmsBackLinkViewModel(IPageViewModel pageViewModel, CMSPageComponent cmsPageComponent)
+        {
+            _cmsPageComponent = cmsPageComponent ?? throw new ArgumentNullException(nameof(cmsPageComponent));
+            _linkId = $"back-link-{pageViewModel.pagename}";
+        }
+
+        public bool HasContent
+        {
+            get
+            {
+                return BackLink.HasContent
+                    && !string.IsNullOrEmpty(LinkId);
+            }
+        }
+
+        public CMSSimpleLink BackLink
+        {
+            get
+            {
+
+                if (_cmsPageComponent.BackLink?.HasContent == true)
+                {
+                    return _cmsPageComponent.BackLink;
+                }
+                else
+                {
+                    return new CMSSimpleLink
+                    {
+                        LinkText = "Back",
+                        LinkUrl = "javascript:history.go(-1);"
+                    };
+                }
+
+            }
+        }
+
+        public string LinkId
+        {
+            get
+            {
+                return this._linkId;
+            }
+        }
+    }
+}
