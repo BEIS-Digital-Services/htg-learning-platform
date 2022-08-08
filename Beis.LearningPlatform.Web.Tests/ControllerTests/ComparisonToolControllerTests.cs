fd1743f193d466f604f574f24dc9680bb8c39599
Sunil D'Souza <sunil.dsouza@angloamerican.com>
2022-08-08 08:47:10 +0100

    Fixed unit tests

--- Beis.LearningPlatform.Web.Tests/ControllerTests/ComparisonToolControllerTests.cs
+++ Beis.LearningPlatform.Web.Tests/ControllerTests/ComparisonToolControllerTests.cs
@@ -17,7 +17,7 @@ namespace Beis.LearningPlatform.Web.Tests.ControllerTests
         private readonly Mock<HttpContext> _httpContext = new();
         private readonly Mock<HttpRequest> _httpRequest = new();
         private IProductCategoryDisplaySettings _productCategoryDisplaySettings;
-
+        
         #region Test Values 
 
         private const string TestValueRequestReferer = "_requestReferer";
@@ -123,6 +123,15 @@ namespace Beis.LearningPlatform.Web.Tests.ControllerTests
             _productCategoryDisplaySettings = new ProductCategoryDisplaySettings(_ctDisplayOptions);
             _comparisonToolService.Setup(x => x.GetApprovedProductsFromApprovedVendors())
                 .Returns(Task.FromResult(TestValueComparisonToolProducts));
+
+            var displaySettings = new List<CMSComparisonToolSearchTag>()
+            {
+                new () { systemId = 1, systemName = "accounting", displayName = "DIGITAL ACCOUNTING SOFTWARE", friendlyDisplayName = "Digital Accounting", isActive = true },
+                new () { systemId = 2, systemName = "crm", displayName = "CUSTOMER RELATIONSHIP MANAGEMENT (CRM) SOFTWARE", friendlyDisplayName = "Customer Relationship Management", isActive = true },
+                new () { systemId = 3, systemName = "ecommerce", displayName = "ECOMMERCE", friendlyDisplayName= "eCommerce", isActive = true },
+                new () { systemId = 4, systemName = "cyberSecurity", displayName = "CYBER SECURITY", friendlyDisplayName = "Cyber Security" , isActive = true},
+            };
+            _cmsService.Setup(x => x.GetDisplaySettings()).ReturnsAsync(displaySettings);
         }
 
         [Test]
@@ -178,9 +187,15 @@ namespace Beis.LearningPlatform.Web.Tests.ControllerTests
             Assert.IsNotNull(viewModel.products);
             Assert.IsTrue(viewModel.products.Any());
 
+            var displaySettings = new List<CMSComparisonToolSearchTag>()
+            {
+                new () { systemId = 2, systemName = "crm", displayName = "CUSTOMER RELATIONSHIP MANAGEMENT (CRM) SOFTWARE", friendlyDisplayName = "Customer Relationship Management", isActive = true },
+                new () { systemId = 1, systemName = "accounting", displayName = "DIGITAL ACCOUNTING SOFTWARE", friendlyDisplayName = "Digital Accounting", isActive = true },
+                new () { systemId = 4, systemName = "cyberSecurity", displayName = "CYBER SECURITY", friendlyDisplayName = "Cyber Security" , isActive = true},
+            };
 
             var productCategoryIdList = productCategoryNames.Split(',').Select(categoryName =>
-                _productCategoryDisplaySettings.DisplaySettings.FirstOrDefault(_ => _.name == categoryName).id.ToString()).ToList();
+                displaySettings.FirstOrDefault(_ => _.systemName == categoryName).systemId.ToString()).ToList();
 
             var productTypeList = viewModel.products.Select(x => x.product_type.ToString()).ToList();
 
@@ -267,7 +282,7 @@ namespace Beis.LearningPlatform.Web.Tests.ControllerTests
             var controller = CreateController();
             var tempData = new Mock<ITempDataDictionary>();
             controller.TempData = tempData.Object;
-
+            
             var viewResult = await controller.CompareProducts(productCategoryIds, productIds) as ViewResult;
 
             AssertCompareProducts(viewResult, productIds, true);
