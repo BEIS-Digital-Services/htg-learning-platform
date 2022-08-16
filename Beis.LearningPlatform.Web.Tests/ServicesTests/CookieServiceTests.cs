cf4de301eff70d21827e24cf39d47ef321048e73
giruvagen <francis.obrien@opencastsoftware.com>
2022-08-16 15:43:39 +0100

    updates cookies service test

--- Beis.LearningPlatform.Web.Tests/ServicesTests/CookieServiceTests.cs
+++ Beis.LearningPlatform.Web.Tests/ServicesTests/CookieServiceTests.cs
@@ -32,18 +32,80 @@ namespace Beis.LearningPlatform.Web.Tests.ServicesTests
             _fixture = new Fixture();
             _fileSystem = new Mock<IFileSystem>();
             _logger = new Mock<ILogger<DiagnosticToolController>>();
-            _cmsLogger = new();
+            _cmsLogger =  new Mock<ILogger<CmsService>>();
             _Configuration = new Mock<IConfiguration>();
             _CmsService = new Mock<ICmsService>();
             _FeedbackService = new Mock<IFeedbackService>();
             _CookieService = new Mock<ICookieService>();
             _MakeApiCallService = new StrapiMakeApiCallMockService();
-            _httpContextAccessor = new();
+            _httpContextAccessor = new Mock<IHttpContextAccessor>();
 
             cmsService = new CmsService(_cmsLogger.Object, _cmsOptions, _MakeApiCallService);
             cookieService = new CookieService(_cookieNamesOptions, _httpContextAccessor.Object);
 
             _TempData = new Mock<ITempDataDictionary>();
+        }
+
+        [Test]
+        public void Should_set_cookie()
+        {
+            try
+            {
+                cookieService.ProcessCookie("HTG", true);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Expected no exception, got: " + e.Message);
+            }
+            
+        }
+        
+        [Test]
+        public void Should_set_banner_closed_cookie()
+        {
+            try
+            {
+                cookieService.ProcessCookie("close", true);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Expected no exception, got: " + e.Message);
+            }
+            
+        }
+
+        [Test]
+        public void Should_fail_invalid_cookie_name()
+        {
+            try
+            {
+                cookieService.ProcessCookie(String.Empty, true);
+                Assert.Fail("Expect Argument Exception, none thrown");
+            }
+            catch (ArgumentException e)
+            {
+                e.Should().BeOfType<ArgumentException>();
+                Assert.Pass();
+            }
+        }
+
+        [Test]
+        public void Saves_cookies_prefs()
+        {
+            cookieService.SaveCookiesPreferences(new SaveCookiePreferenceModel());
+        }
+
+        [Test]
+        public void Should_return_valid_cookie_pref_model()
+        {
+            httpContext.Request.Cookies = new Mock<IRequestCookieCollection>().Object;
+            httpContext.Session = new Mock<ISession>().Object;
+            _httpContextAccessor.Setup(x => x.HttpContext).Returns(httpContext);
+
+            cookieService = new CookieService(_cookieNamesOptions, _httpContextAccessor.Object);
+
+            var result = cookieService.GetUserCookiePreferences();
+            result.Should().BeOfType<UserCookiePreferencesModel>();
         }
     }
 }
