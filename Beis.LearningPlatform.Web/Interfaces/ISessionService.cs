18a1adf3dab2d6daff17276b62cf560519f035b6
giruvagen <francis.obrien@opencastsoftware.com>
2022-08-22 09:14:29 +0100

    fix naming in interface

--- Beis.LearningPlatform.Web/Interfaces/ISessionService.cs
+++ Beis.LearningPlatform.Web/Interfaces/ISessionService.cs
@@ -2,9 +2,9 @@ namespace Beis.LearningPlatform.Web.Interfaces;
 
 public interface ISessionService
 {
-    public void Set(string key, object value, HttpContext currentContext);
+    public void Set(string key, object value, HttpContext httpContext);
 
-    public bool TryGet<T>(string key, HttpContext currentContext, out T value);
+    public bool TryGet<T>(string key, HttpContext httpContext, out T value);
 
     public bool HasValidSession(HttpContext httpContext);
 
