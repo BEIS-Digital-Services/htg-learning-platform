6f0f68c161580f76fc4302d318ea0eb2a86ab594
James King <hello@jamesmichaelking.com>
2022-10-11 10:35:31 +0100

    Prevent default action on the link

--- Beis.LearningPlatform.Web/wwwroot/js/comparisontool.js
+++ Beis.LearningPlatform.Web/wwwroot/js/comparisontool.js
@@ -57,6 +57,7 @@ function manageProducts(e) {
 
     toggleGroupCompareButtons(this);
     toggleFormDivElements(this);
+    e.preventDefault()
 }
 
 function toggleGroupCompareButtons(currentButton) {
