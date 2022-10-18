3033a8da92620a5ed1704000d1de0ac5555cd357
Thomas Poulsen <thomas.poulsen@broadlight.io>
2022-10-18 11:24:50 +0100

    PS-854: home page warning banner
    (cherry picked from commit 3cc109d471e9407db70f3800a735033c5bb1aac9)

---
+++ Beis.LearningPlatform.Web/Models/CmsHomePageWarningViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Beis.LearningPlatform.Web.Models
+{
+    public class CmsHomePageWarningViewModel
+    {
+        private readonly CMSPageComponent _cmsPageComponent;
+
+        public CmsHomePageWarningViewModel(CMSPageComponent cmsPageComponent)
+        {
+            _cmsPageComponent = cmsPageComponent;
+        }
+
+        public string Header { get { return _cmsPageComponent?.header; } }
+        public string Intro { get { return _cmsPageComponent?.intro; } }
+        public string IntroHtml { get; set; }
+
+        public bool HasContent
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(Header)
+                    && !string.IsNullOrEmpty(Intro);
+            }
+        }
+    }
+}
---
+++ Beis.LearningPlatform.Web/ViewComponents/CmsHomePageWarningViewComponent.cs
@@ -0,0 +1,20 @@
+﻿namespace Beis.LearningPlatform.Web.ViewComponents
+{
+    public class CmsHomePageWarningViewComponent : ViewComponent
+    {
+        private readonly MarkdownPipeline _markdownPipeline;
+
+        public CmsHomePageWarningViewComponent(MarkdownPipeline markdownPipeline)
+        {
+            _markdownPipeline = markdownPipeline;
+        }
+
+        public IViewComponentResult Invoke(CMSPageComponent cmsPageComponent)
+        {
+            var viewModel = new CmsHomePageWarningViewModel(cmsPageComponent);
+            viewModel.IntroHtml = Markdown.ToHtml(viewModel.Intro, _markdownPipeline);
+
+            return View(viewModel);
+        }
+    }
+}
