18f2592c539314b04da61bcfb147639a9937f502
Thomas Poulsen <thomas.poulsen@broadlight.io>
2022-10-20 10:11:47 +0100

    PS-855: Global warning banner

---
+++ Beis.LearningPlatform.Web/Models/GlobalWarningMessageModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Beis.LearningPlatform.Web.Models;
+
+public class GlobalWarningMessageModel
+{
+    public string Header { get; set; }
+    public string Body { get; set; }
+    public bool Enabled { get; set; }
+    public string BodyHtml { get; set; }
+}
